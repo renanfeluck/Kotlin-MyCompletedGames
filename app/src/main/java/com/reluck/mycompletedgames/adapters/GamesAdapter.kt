662b168088b1b9513ad831cecbda6d820915ea7e
Renan <renan.luckow@mjv.com.br>
2019-07-01 23:35:01 -0300

    MaterialDesign Dialog

--- app/src/main/java/com/reluck/mycompletedgames/adapters/GamesAdapter.kt
+++ app/src/main/java/com/reluck/mycompletedgames/adapters/GamesAdapter.kt
@@ -1,17 +1,21 @@
 package com.reluck.mycompletedgames.adapters
 
 import android.content.Context
+import android.content.DialogInterface
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.reluck.mycompletedgames.R
 import com.reluck.mycompletedgames.data.db.GameDatabase
 import com.reluck.mycompletedgames.data.db.entity.Game
 import com.reluck.mycompletedgames.repository.GameRepository
 import com.reluck.mycompletedgames.ui.GameViewModel
+import kotlinx.android.synthetic.main.game_row.view.*
 import kotlinx.coroutines.Dispatchers
 
 class GamesAdapter(var games: MutableList<Game>, val gameViewModel: GameViewModel): RecyclerView.Adapter<GamesAdapter.ViewHolder>() {
@@ -29,10 +33,21 @@ class GamesAdapter(var games: MutableList<Game>, val gameViewModel: GameViewMode
         holder.gameTime.text = games[position].time
         holder.gameCategory.text = games[position].category
 
-        holder.itemView.setOnClickListener{
-            gameViewModel.delete(games[position])
-            notifyDataSetChanged()
+        holder.itemView.imgDelete.setOnClickListener{
+
+            MaterialAlertDialogBuilder(holder.itemView.context)
+                .setTitle("Do you want to delete this entry?")
+                .setPositiveButton("Yes", DialogInterface.OnClickListener { dialog, which ->  delete(position) })
+                .setNegativeButton("Cancel", null)
+                .show();
+
         }
+
+    }
+
+    fun delete(position: Int) {
+        gameViewModel.delete(games[position])
+        notifyDataSetChanged()
     }
 
     class ViewHolder(itemView: View, context: Context): RecyclerView.ViewHolder(itemView) {
@@ -40,6 +55,7 @@ class GamesAdapter(var games: MutableList<Game>, val gameViewModel: GameViewMode
         val gameTitle: TextView = itemView.findViewById(R.id.textTitle)
         val gameTime: TextView = itemView.findViewById(R.id.textTime)
         val gameCategory: TextView = itemView.findViewById(R.id.textCategory)
+        val imgDelete: ImageView = itemView.findViewById(R.id.imgDelete)
 
     }
 }
